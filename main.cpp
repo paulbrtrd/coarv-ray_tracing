2c0186731682265413f68da31e238409f95640cd
Paul <paul.bertrand@eleves.ec-nantes.fr>
2020-02-11 16:00:17 +0100

    Chapitre 6

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "vec3.h"
 #include "sphere.h"
 #include "hitableList.h"
+#include "camera.h"
+
 #include <float.h>
 #define DEBUG 0
 
@@ -24,6 +26,7 @@ int main () {
     
     int nx = 200;
     int ny = 100;
+    int ns = 100;
     new_file << "P3\n" << nx << " " << ny << "\n255\n";
 
     Vec3 lower_left_corner(-2.0, -1.0, -1.0);
@@ -35,15 +38,18 @@ int main () {
     list[0] = new Sphere(Vec3(0,0,-1), 0.5);
     list[1] = new Sphere(Vec3(-0, -100.5, -1), 100);
     Hitable * world = new Hitable_list(list, 2);
+    Camera cam;
     for(int j=ny-1; j>=0; j--) {
         for (int i=0; i<nx; i++) {
-        
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            Ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            //Vec3 p = r.point_at_parameter(2.0);
-            Vec3 col = color(r, world);
-
+            Vec3 col(0,0,0);
+            for (int s=0; s<ns; s++) {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+                Ray r = cam.get_ray(u,v);
+                Vec3 p = r.point_at_parameter(2.0);
+                col+=color(r, world);
+            }
+            col/=float(ns);
             int ir = int(255.99*col.r());
             int ig = int(255.99*col.g());
             int ib = int(255.99*col.b());
